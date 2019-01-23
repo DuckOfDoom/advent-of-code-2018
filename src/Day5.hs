2801a58750dbab734615dc42d0c20826593a1895
Vladimir Yanovsky <horaxlfa@gmail.com>
2019-01-23 17:54:33 +0300

    Day5 wip

--- src/Day5.hs
+++ src/Day5.hs
@@ -12,15 +12,33 @@ import qualified Data.Text as T
 day5 :: IO Text
 day5 = do
   input1 <- readFile "input_day5_1.txt"
-  pure $ (Utils.showT . length . process . T.unpack) input1
+  writeFile "out.txt" $ (T.pack . fullyReact . T.unpack) input1
+  _ <- Utils.executeWithTimer "process" $ (pure . length . process . T.unpack) input1
+  var2 <- Utils.executeWithTimer "fullyReact" $ (pure . length . fullyReact . T.unpack) input1
+  pure $ (Utils.showT var2)
+
+fullyReact :: [Char] -> [Char]
+fullyReact xs = react (length xs) xs
+  where
+    react prevLength ys =
+      let
+        current = process2 ys
+        currLength = length current
+        in
+        -- trace (Utils.showT prevLength) $
+        if currLength == prevLength
+          then current
+          else react currLength current
+
+test = Utils.executeWithTimer "derp" $ (pure . fullyReact) "dabAcCaCBAcCcaDA"
 
 process :: [Char] -> [Char]
-process xs = let 
+process xs = let
   process' vals = runState (processOnce vals) (0, [])
   (_, (count, processed)) = process' xs
   in if count /= 0 then process processed else processed
 
--- This can be sped up if we scan by 4 values and collapse middle four. 
+-- This can be sped up if we scan by 4 values and collapse middle two.
 -- But the function might be ugly
 processOnce :: [Char] -> State (Int, [Char]) [Char]
 processOnce (x:y:xs) = do
@@ -28,13 +46,6 @@ processOnce (x:y:xs) = do
   if isPolar x y
     then put (count + 1, values) >> processOnce xs
     else put (count, x:values) >> processOnce (y:xs)
-  where
-    isPolar :: Char -> Char -> Bool
-    isPolar a b
-      | a == b = False
-      | toLower a == toLower b = True
-      | otherwise = False
-
 processOnce [x] = do
   (count, values) <- get
   put (count, reverse $ x:values)
@@ -42,3 +53,21 @@ processOnce [x] = do
 
 processOnce [] = panic "Empty list, dude!"
 
+process2 :: [Char] -> [Char]
+process2 (x1:x2:x3:xs)
+  | isPolar x1 x2 = process2 (x3:xs)
+  | isPolar x2 x3 = process2 (x1:xs)
+  | otherwise = x1 : process2 (x2:x3:xs)
+
+process2 (x1:x2:xs)
+  | isPolar x1 x2 = process2 xs
+  | otherwise = x1:x2:process2 xs
+
+process2 [x] = [x]
+process2 [] = []
+
+isPolar :: Char -> Char -> Bool
+isPolar a b
+  | a == b = False
+  | toLower a == toLower b = True
+  | otherwise = False
--- src/Utils.hs
+++ src/Utils.hs
@@ -2,46 +2,48 @@ module Utils where
 
 import Protolude
 -- import GHC.Show
+import System.CPUTime (getCPUTime)
+
 import qualified Data.Text as T
 
-import Data.HashMap.Strict (HashMap)
+import           Data.HashMap.Strict (HashMap)
 import qualified Data.HashMap.Strict as HM
 
 showT :: (Show a) => a -> Text
 showT = T.pack . show
 
 modifyOrAdd ::(Eq k, Hashable k) => k -> (v -> v) -> v -> HashMap k v -> HashMap k v
-modifyOrAdd key func value hmap 
+modifyOrAdd key func value hmap
   | HM.member key hmap = HM.adjust func key hmap
   | otherwise = HM.insert key value hmap
 
 -- makes pairs of all non-equal elements in a list.
--- e.g. mkParis "abc" = [('a','b'),('a','c'),('b','a'),('b','c'),('c','a'),('c','b')] 
+-- e.g. mkParis "abc" = [('a','b'),('a','c'),('b','a'),('b','c'),('c','a'),('c','b')]
 mkPairsSelf :: (Eq a) => [a] -> [(a, a)]
-mkPairsSelf l = concatMap (\e -> mkPairs' e l) l 
+mkPairsSelf l = concatMap (\e -> mkPairs' e l) l
   where
     mkPairs' :: (Eq a) => a -> [a] -> [(a,a)]
-    mkPairs' k (y:ys) = 
-      if k == y 
-        then 
+    mkPairs' k (y:ys) =
+      if k == y
+        then
           mkPairs' k ys
         else
           (k, y) : mkPairs' k ys
     mkPairs' _ [] = []
-    
+
 -- same as above but with two different lists and with duplicates
 mkPairs :: (Eq a) => [a] -> [a] -> [(a, a)]
 mkPairs x = concatMap (\e -> mkPairs' e x)
   where
     mkPairs' :: (Eq a) => a -> [a] -> [(a,a)]
     mkPairs' k (y:ys) = (k, y) : mkPairs' k ys
-    mkPairs' _ [] = []
+    mkPairs' _ []     = []
 
 -- groups entries by key using hashmap
 groupBy :: (Eq k, Hashable k) => (a -> k) -> [a] -> [[a]]
-groupBy selector xs = 
+groupBy selector xs =
   let m = foldl (\hm v -> modifyOrAdd (selector v) (v :) [v] hm) (HM.empty :: HashMap k [a]) xs
-  in 
+  in
     map snd . HM.toList $ m
 
 countOccurences :: (Eq a, Hashable a) => [a] -> HashMap a Int
@@ -49,6 +51,16 @@ countOccurences = foldl (\hm' x -> modifyOrAdd x (+1) 1 hm') HM.empty
 
 countOccurences2 :: (Eq a, Hashable a) => [[a]] -> HashMap a  Int
 countOccurences2 = foldl (\hm xs -> HM.unionWith (+) hm (countOccurences xs)) HM.empty
-      
+
 maxBy :: (Ord b) => (a -> b) -> [a] -> a
 maxBy selector = maximumBy (\a1 a2 -> selector a1 `compare` selector a2)
+
+executeWithTimer :: Text -> IO t -> IO t
+executeWithTimer name f = do
+  start <- getCPUTime
+  v <- f >>= evaluate
+  end <- getCPUTime
+  let
+    timeDiff = fromIntegral (end - start) / (10^(12 :: Integer))
+  putStrLn $ mconcat ["Job \"", name, "\" was completed in ",  (showT (timeDiff :: Double)), " seconds"]
+  pure v
